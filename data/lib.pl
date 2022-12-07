fdc865f0b360bf4165c5c0a1e715b455b4e6c93b
slowe <299787+slowe@users.noreply.github.com>
2022-12-07 09:31:11 +0000

    Fix UTF8 issues in tests

--- data/lib.pl
+++ data/lib.pl
@@ -254,7 +254,7 @@ sub parseOpeningHours {
 		$str =~ s/([^0-9]) \&amp\; ([^0-9])/$1, $2/g;
 		$str =~ s/ ?\([^\)]+\)//g;
 		$str =~ s/\//, /g;
-		$str =~ s/[—]/-/g;
+		$str =~ s/[—–]/-/g;
 		$str =~ s/24 hours/00:00-24:00/g;
 
 		# Convert "weekdays" or "weekends" into day ranges
