273b46fcf59de9c9f2302ab0fcdedf29abb75da8
slowe <299787+slowe@users.noreply.github.com>
2023-01-31 15:39:25 +0000

    Add Hinkley and Bosworth

---
+++ data/scrapers/hinckley_and_bosworth_council.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+use lib "./";
+use utf8;
+use Data::Dumper;
+use Web::Scraper;
+require "lib.pl";
+binmode STDOUT, 'utf8';
+
+# Get the file to process
+$file = $ARGV[0];
+
+# If the file exists
+if(-e $file){
+
+	# Open the file
+	open(FILE,"<:utf8",$file);
+	@lines = <FILE>;
+	close(FILE);
+	$str = join("",@lines);
+
+	# Build a web scraper
+	my $warmspaces = scraper {
+		process '.content li', "warmspaces[]" => scraper {
+			process '*', 'content' => 'HTML';
+		};
+	};
+	my $res = $warmspaces->scrape( $str );
+
+	for($i = 0; $i < @{$res->{'warmspaces'}}; $i++){
+		
+		$row = $res->{'warmspaces'}[$i];
+		
+		$row->{'content'} = parseText($row->{'content'});
+		if($row->{'content'} =~ /(.*?)\, ([^\:]*?): (.*)/){
+			$d->{'title'} = $1;
+			$d->{'address'} = $2;
+			$d->{'hours'} = parseOpeningHours({'_text'=>$3});
+		}
+		push(@entries,makeJSON($d,1));
+	}
+
+	open(FILE,">:utf8","$file.json");
+	print FILE "[\n".join(",\n",@entries)."\n]";
+	close(FILE);
+
+	print $file.".json";
+
+}else{
+
+	print "";
+
+}
+
+
+sub trimHTML {
+	my $str = $_[0];
+	$str =~ s/(<br ?\/?>|<p>)/\n /g;
+	$str =~ s/<[^\>]+>//g;
+	$str =~ s/(^[\s\t\n\r]+|[\s\t\n\r]+$)//g;
+	$str =~ s/[\n\r]{2,}/\n/g;
+	$str =~ s/[\s\t]{2,}/ /g;
+	return $str;
+}
