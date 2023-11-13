488ef3a8b94b929532951819001bd46f5727eb1a
slowe <299787+slowe@users.noreply.github.com>
2023-11-13 18:27:25 +0000

    Use Bradford Council's geocoding

--- data/scrapers/bradford_council.pl
+++ data/scrapers/bradford_council.pl
@@ -2,6 +2,7 @@
 
 use lib "./";
 use utf8;
+use JSON::XS;
 use Web::Scraper;
 use Data::Dumper;
 require "lib.pl";
@@ -61,6 +62,19 @@ if(-e $file){
 			@lines = <FILE>;
 			close(FILE);
 			$html = join("",@lines);
+			
+			
+			
+			$urls = {};
+
+			if($str =~ /var WPBDP_googlemaps_data = \{"map_0":(.*?)\};\n/){
+				@locs = @{parseJSON($1)->{'locations'}};
+				
+				for($l = 0; $l < @locs; $l++){
+					$urls->{$locs[$l]{'url'}} = $locs[$l];
+				}
+			}
+
 
 			$res = $warmspaces->scrape( $html );
 			
@@ -72,14 +86,19 @@ if(-e $file){
 					$d->{'address'} =~ s/\, +\,/\,/g;
 					if($d->{'hours'}){ $d->{'hours'} = parseOpeningHours({'_text'=>$d->{'hours'}}); }
 					if(!$d->{'hours'}{'opening'}){ delete $d->{'hours'}{'opening'}; }
-
+					
+					if($urls->{$d->{'url'}}){
+						if($urls->{$d->{'url'}}{'geolocation'} && !$d->{'lat'}){
+							$d->{'lat'} = $urls->{$d->{'url'}}{'geolocation'}{'lat'};
+							$d->{'lon'} = $urls->{$d->{'url'}}{'geolocation'}{'lng'};
+						}
+					}
 					push(@entries,makeJSON($d,1));
 				}
 			}
-
 		}
-
 	}
+
 
 	warning("\tSaved to $file.json\n");
 	open(FILE,">:utf8","$file.json");
@@ -94,3 +113,19 @@ if(-e $file){
 
 }
 
+
+
+
+sub parseJSON {
+	my $str = $_[0];
+	my ($json);
+	# Error check for JS variable
+	$str =~ s/[^\{]*var [^\{]+ = //g;
+	if(!$str){ $str = "{}"; }
+	eval {
+		$json = JSON::XS->new->decode($str);
+	};
+	if($@){ error("\tInvalid output in $file.\n"); $json = {}; }
+	
+	return $json;
+}
