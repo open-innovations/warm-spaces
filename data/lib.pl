8ad20cf9ad4e062a1fc7fc89d8ff7bbb0bd44a26
slowe <299787+slowe@users.noreply.github.com>
2024-11-15 00:21:26 +0000

    Add Boston Borough Council

--- data/lib.pl
+++ data/lib.pl
@@ -6,38 +6,41 @@ use Encode;
 binmode STDOUT, 'utf8';
 binmode STDERR, 'utf8';
 
-my %colours = (
-	'black'=>"\033[0;30m",
-	'red'=>"\033[0;31m",
-	'green'=>"\033[0;32m",
-	'yellow'=>"\033[0;33m",
-	'blue'=>"\033[0;34m",
-	'magenta'=>"\033[0;35m",
-	'cyan'=>"\033[0;36m",
-	'white'=>"\033[0;37m",
-	'none'=>"\033[0m"
-);
-
 sub msg {
 	my $str = $_[0];
-	my $dest = $_[1]||STDOUT;
+	my $dest = $_[1]||"STDOUT";
+	
+	my %colours = (
+		'black'=>"\033[0;30m",
+		'red'=>"\033[0;31m",
+		'green'=>"\033[0;32m",
+		'yellow'=>"\033[0;33m",
+		'blue'=>"\033[0;34m",
+		'magenta'=>"\033[0;35m",
+		'cyan'=>"\033[0;36m",
+		'white'=>"\033[0;37m",
+		'none'=>"\033[0m"
+	);
 	foreach my $c (keys(%colours)){ $str =~ s/\< ?$c ?\>/$colours{$c}/g; }
-	print $dest $str;
+	if($dest eq "STDERR"){
+		print STDERR $str;
+	}else{
+		print STDOUT $str;
+	}
 }
 
 sub error {
 	my $str = $_[0];
 	$str =~ s/(^[\t\s]*)/$1<red>ERROR:<none> /;
-	foreach my $c (keys(%colours)){ $str =~ s/\< ?$c ?\>/$colours{$c}/g; }
-	msg($str,STDERR);
+	msg($str,"STDERR");
 }
 
 sub warning {
 	my $str = $_[0];
-	$str =~ s/(^[\t\s]*)/$1$colours{'yellow'}WARNING:$colours{'none'} /;
-	foreach my $c (keys(%colours)){ $str =~ s/\< ?$c ?\>/$colours{$c}/g; }
-	print STDERR $str;
+	$str =~ s/(^[\t\s]*)/$1<yellow>WARNING:<none> /;
+	msg($str,"STDERR");
 }
+
 sub parseText {
 	my $str = $_[0];
 	$str =~ s/<br ?\/?>/ /g;
---
+++ data/scrapers/boston_borough_council.pl
@@ -0,0 +1,149 @@
+#!/usr/bin/perl
+
+use lib "./";
+use utf8;
+use Data::Dumper;
+use Web::Scraper;
+require "lib.pl";
+binmode STDOUT, 'utf8';
+
+# Get the file to process
+$file = $ARGV[0];
+
+# If the file exists
+if(-e $file){
+
+	# Open the file
+	open(FILE,"<:utf8",$file);
+	@lines = <FILE>;
+	close(FILE);
+	$str = join("",@lines);
+
+	# Build a scraper to find the pages of results
+	my $pagescrape = scraper {
+		process '.paging__link', "url" => '@HREF';
+		process '.paging__link', "page" => '@DATA-PAGE';
+	};
+	
+	# Build a web scraper to parse a page of results
+	my $warmspaces = scraper {
+		process '.item--article', "warmspaces[]" => scraper {
+			process '.item__link', 'url' => '@HREF';
+			process '.item__link', 'title' => 'TEXT';
+		}
+	};
+	# Build a web scraper to parse a page of results
+	my $entry = scraper {
+		process '.a-heading__title', 'title' => 'TEXT';
+		process '.a-intro__content', 'description' => 'TEXT';
+		process '.a-body.a-body--default', 'body' => 'HTML';
+	};
+
+
+	my @pp = ({'page'=>1,'content'=>$str},$pagescrape->scrape($str));
+	my ($res,@entries,@results);
+	@entries = ();
+
+	for(my $p = 0; $p < @pp; $p++){
+		if($pp[$p]{'url'}){
+			$rfile = "raw/boston-council-page-$pp[$p]{'page'}.html";
+			warning("\tGetting details for $p = page $pp[$p]{'page'} (<cyan>$rfile<none>)\n");
+			# Keep cached copy of individual URL
+			$age = getFileAge($rfile);
+			if($age >= 86400 || -s $rfile == 0){
+				warning("\tSaving <green>$purl<none> to <cyan>$rfile<none>\n");
+				# Download the section
+				`curl -s --insecure --compressed -o $rfile "$pp[$p]{'url'}"`;
+			}
+			open(FILE,"<:utf8",$rfile);
+			@lines = <FILE>;
+			close(FILE);
+			$pp[$p]{'content'} = join("",@lines);
+		}
+
+		$res = $warmspaces->scrape( $pp[$p]{'content'} );
+		push(@results,@{$res->{'warmspaces'}});
+	}
+
+	# Parse each result
+	for($i = 0; $i < @results; $i++){
+		$rfile = "raw/boston-council-result-$i.html";
+		# Keep cached copy of individual URL
+		$age = getFileAge($rfile);
+		if($age >= 86400 || -s $rfile == 0){
+			warning("\tSaving <green>$purl<none> to <cyan>$rfile<none>\n");
+			# Download the section
+			`curl -s --insecure --compressed -o $rfile "$results[$i]{'url'}"`;
+		}
+		msg("Reading <cyan>$rfile<none>\n");
+		open(FILE,"<",$rfile);
+		@lines = <FILE>;
+		close(FILE);
+		$html = join("",@lines);
+
+		$res = $entry->scrape( $html );
+#		$d->{'title'} = $results[$i]->{'title'};
+#		$d->{'url'} = $results[$i]->{'url'}; 
+		
+		if($res->{'description'}){
+			$res->{'description'} = trimText($res->{'description'});
+		}
+		$res->{'body'} =~ s/\&nbsp\;/ /g;
+		$res->{'body'} =~ s/\&apos\;/\'/g;
+
+		if($res->{'body'} =~ s/<h2>Organisation:<\/h2><p>(.*?)<\/p>(.*?)<[^\>]+>Contact:?<\/[^\>]+>(.*?)<[^\>]+>Details:?<\/[^\>]+>(.*)//s){
+			$p1 = $1;
+			$p2 = $2;
+			$p3 = $3;
+			$p4 = $4;
+			$contact = $p3;
+			$res->{'address'} = trimHTML($p1);
+			$res->{'hours'} = parseOpeningHours({'_text'=>trimHTML($p2.". ".$p4)});
+			if(!$d->{'hours'}{'opening'}){ delete $d->{'hours'}; }
+			if(!$res->{'hours'}{'opening'}){
+				$res->{'description'} = $res->{'hours'}{'_text'};
+				delete $res->{'hours'};
+			}
+			if($contact =~ s/<p><a href="([^\"]+\@[^\"]+)"[^\>]*>(.*)<\/a><\/p>//){
+				$email = $1;
+				$to = $2;
+				$email =~ s/^https?\:\/\///g;
+				if($to =~ /email/i){
+					$to =~ s/^Email //i;
+					$res->{'contact'} .= ($res->{'contact'} ? "; ":"")."Email: $email";
+				}
+				$res->{'contact'} = trimHTML($contact)."; ".$res->{'contact'};
+				$res->{'contact'} =~ s/ ([0-9]{3,}\s?[0-9]{7,})/ Tel: $1/;
+			}
+		}
+		delete $res->{'body'};
+		push(@entries,makeJSON($res,1));
+	}
+	open(FILE,">:utf8","$file.json");
+	print FILE "[\n".join(",\n",@entries)."\n]";
+	close(FILE);
+
+	print $file.".json";
+
+}else{
+
+	print "";
+
+}
+
+
+sub trimHTML {
+	my $str = $_[0];
+	$str =~ s/(<br ?\/?>|<p>)/\n /g;
+	$str =~ s/([^\.])(<\/li>|<\/p>)/$1; /g;
+	$str =~ s/<[^\>]+>//g;
+	$str =~ s/(^[\s\t\n\r]+|[\s\t\n\r]+$)//g;
+	$str =~ s/[\n\r]{2,}/\n/g;
+	$str =~ s/[\s\t]{2,}/ /g;
+	$str =~ s/\;\s\.?\s?\;/\;/g;
+	$str =~ s/ ?\;$//g;
+	$str =~ s/^\. //g;
+	$str =~ s/\s\;\s/\; /g;
+	$str =~ s/\;\s+$//g;
+	return $str;
+}
