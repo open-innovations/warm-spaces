bff0965f0787561f3d82608bd8075894f26c1acb
slowe <299787+slowe@users.noreply.github.com>
2024-11-19 11:07:45 +0000

    Update Bradford Council again

--- data/scrapers/bradford_council.pl
+++ data/scrapers/bradford_council.pl
@@ -3,6 +3,7 @@
 use lib "./";
 use utf8;
 use JSON::XS;
+use Web::Scraper;
 use Data::Dumper;
 require "lib.pl";
 binmode STDOUT, 'utf8';
@@ -18,18 +19,79 @@ if(-e $file){
 	@lines = <FILE>;
 	close(FILE);
 	$str = join("",@lines);
-	
-	if($str =~ /"locations":(\[\{.*\}\])/){
-		@results = @{parseJSON("var locations = ".$1."")};
-		for($i = 0; $i < @results; $i++){
-			$results[$i]->{'lat'} = $results[$i]->{'geolocation'}{'lat'};
-			$results[$i]->{'lon'} = $results[$i]->{'geolocation'}{'lng'};
-			delete $results[$i]->{'geolocation'};
-			delete $results[$i]->{'content'};
-			push(@entries,makeJSON($results[$i],1));
+
+	my $pages = scraper {
+		process 'h2.elementor-heading-title a', 'url[]' =>  '@href';
+	};
+	$pages = $pages->scrape( $str );
+
+	# Build a web scraper
+	my $warmspaces = scraper {
+		process '.wpbdp-listing', "warmspace[]" => scraper {
+			process '.wpbdp-field-organisation_name', 'title' => 'TEXT';
+			process '.wpbdp-field-organisation_name .value a', 'url' => '@HREF';
+			process '.wpbdp-field-address', 'address' => 'TEXT';
+			process '.wpbdp-field-postcode', 'postcode' => 'TEXT';
+			#process '.wpbdp-field-who_is_the_warm_space_contact .value', 'contact' => 'TEXT';
+			#process '.wpbdp-field-telephone_number', 'phone' => 'TEXT';
+			process '.wpbdp-field-opening_times .value', 'hours' => 'TEXT';
+			process '.wpbdp-field-description .value', 'description' => 'TEXT';
+			#process '.wpbdp-field-facilitiesamenities .value', 'facilities' => 'TEXT';
 		}
-	}else{
-		warning("No locations in Bradford HTML\n");
+	};
+
+	for($p = 0; $p < @{$pages->{'url'}}; $p++){
+
+		$url = $pages->{'url'}[$p];
+
+		if($url =~ /.*warm-spaces-directory\/([^\/]*)\/?/){
+			$section = $1;
+			$rfile = "raw/bradford-council-$section.html";
+			warning("\tGetting details for $p = $section (<cyan>$rfile<none>)\n");
+
+			# Keep cached copy of individual URL
+			$age = getFileAge($rfile);
+			if($age >= 86400 || -s $rfile == 0){
+				warning("\tSaving <green>$purl<none> to <cyan>$rfile<none>\n");
+				# Download the section
+				`curl -s --insecure --compressed -o $rfile "$url"`;
+			}
+			open(FILE,"<:utf8",$rfile);
+			@lines = <FILE>;
+			close(FILE);
+			$html = join("",@lines);
+
+			$urls = {};
+
+			if($str =~ /var WPBDP_googlemaps_data = \{"map_0":(.*?)\};\n/){
+				@locs = @{parseJSON($1)->{'locations'}};
+				for($l = 0; $l < @locs; $l++){
+					$urls->{$locs[$l]{'url'}} = $locs[$l];
+				}
+			}
+
+
+			$res = $warmspaces->scrape( $html );
+
+			for($i = 0; $i < @{$res->{'warmspace'}}; $i++){
+				$d = $res->{'warmspace'}[$i];
+				if($d->{'title'}){
+
+					if($d->{'postcode'} && $d->{'address'}){ $d->{'address'} .= ', '.$d->{'postcode'}; }
+					$d->{'address'} =~ s/\, +\,/\,/g;
+					if($d->{'hours'}){ $d->{'hours'} = parseOpeningHours({'_text'=>$d->{'hours'}}); }
+					if(!$d->{'hours'}{'opening'}){ delete $d->{'hours'}{'opening'}; }
+					
+					if($urls->{$d->{'url'}}){
+						if($urls->{$d->{'url'}}{'geolocation'} && !$d->{'lat'}){
+							$d->{'lat'} = $urls->{$d->{'url'}}{'geolocation'}{'lat'};
+							$d->{'lon'} = $urls->{$d->{'url'}}{'geolocation'}{'lng'};
+						}
+					}
+					push(@entries,makeJSON($d,1));
+				}
+			}
+		}
 	}
 
 	warning("\tSaved to $file.json\n");
