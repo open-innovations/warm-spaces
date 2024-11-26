79ab9274babf212d3fdbc2d031316c9c6908ab35
slowe <299787+slowe@users.noreply.github.com>
2024-11-26 19:10:39 +0000

    Update Essex map

--- data/scrapers/essex_map.pl
+++ data/scrapers/essex_map.pl
@@ -23,47 +23,43 @@ if(-e $file){
 
 	# Build a web scraper
 	my $warmspaces = scraper {
-		process 'article.gd_place_tags-warm-spaces, article.gd_placecategory-warm-spaces1', "warmspaces[]" => scraper {
+		process 'article.gd_place_tags-warm-spaces, article.gd_placecategory-warm-spaces1, article.gd_placecategory-warm-spaces', "warmspaces[]" => scraper {
 			process 'h1', 'title' => 'TEXT';
 			process 'a', 'url' => '@HREF';
 			process '*', 'id' => '@ID';
 		}
 	};
 	
-	# Build a web scraper
-	my $pp = scraper {
-		process 'a.page-link', "pages[]" => '@HREF';
-	};
-	my $res = $pp->scrape( $str );
-	my $endpage = 1;
-	for($i = 0; $i < @{$res->{'pages'}}; $i++){
-		if($res->{'pages'}[$i] =~ /page\/([0-9]+)\//){
-			if($1 > $endpage){ $endpage = $1; }
+	# Loop over pages
+	$next = "placeholder";
+	$n = 0;
+	while($next){
+		if($n>0){
+			# Get new page here
+			$rfile = "raw/essex_map-page$n.html";
+			# Keep cached copy of individual URL
+			$age = getFileAge($rfile);
+			if($age >= 86400 || -s $rfile == 0){
+				warning("\tSaving $next to <cyan>$rfile<none>\n");
+				# For each entry we now need to get the sub page to find the location information
+				`curl '$next' -o $rfile -s --insecure -L --compressed -H 'Upgrade-Insecure-Requests: 1' -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0" -H "Accept-Language: en-GB,en;q=0.5" -H "Accept-Encoding: gzip, deflate"`;
+			}
+			open(FILE,"<:utf8",$rfile);
+			@lines = <FILE>;
+			close(FILE);
+			$str = join("",@lines);
 		}
-	}
-	warning("\tEssex map has <yellow>$endpage<none> pages\n");
-
-
-	for($i = 1; $i <= $endpage; $i++){
-
-		$url = $res->{'pages'}[0];
-		$url =~ s/page\/([0-9]+)\//page\/$i/;
-		$rfile = "raw/essex_map_page_$i.html";
-		# Keep cached copy of individual URL
-		$age = getFileAge($rfile);
-		if($age >= 86400 || -s $rfile == 0){
-			warning("\tSaving <green>$url<none> to <cyan>$rfile<none>\n");
-			# For each entry we now need to get the sub page to find the location information
-			`curl '$url' -o $rfile -s --insecure -L --compressed -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-GB,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Upgrade-Insecure-Requests: 1'`;
+		# Get next page link
+		if($str =~ /"next page-link".*?href="([^\"]+)"/){
+			$n++;
+			$next = $1;
+			$next =~ s/\&\#038;/\&/g;
+		}else{
+			$next = "";
 		}
-		warning("\tReading <cyan>$rfile<none>\n");
-		open(FILE,"<:utf8",$rfile);
-		@lines = <FILE>;
-		close(FILE);
-		$str = join("",@lines);
-		
+
+		# Parse listing page
 		$warm = $warmspaces->scrape( $str );
-		
 		for($j = 0; $j < @{$warm->{'warmspaces'}}; $j++){
 			$d = $warm->{'warmspaces'}[$j];
 			$d->{'id'} =~ s/post-//g;
@@ -76,7 +72,7 @@ if(-e $file){
 				if($age >= 86400 || -s $rfile == 0){
 					warning("\tSaving <green>$url<none> to <cyan>$rfile<none>\n");
 					# For each entry we now need to get the sub page to find the location information
-					`curl '$url' -o $rfile -s --insecure -L --compressed -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-GB,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Upgrade-Insecure-Requests: 1'`;
+					`curl '$url' -o $rfile -s --insecure -L --compressed -H 'Upgrade-Insecure-Requests: 1' -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0" -H "Accept-Language: en-GB,en;q=0.5" -H "Accept-Encoding: gzip, deflate"`;
 				}
 				open(FILE,"<:utf8",$rfile);
 				@lines = <FILE>;
@@ -113,7 +109,6 @@ if(-e $file){
 					delete $d->{'id'};
 				}
 				push(@entries,makeJSON($d,1));
-
 			}
 		}
 	}
