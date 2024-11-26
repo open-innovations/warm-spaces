b561845e5a79d3989324762622332c1d18784f0d
slowe <299787+slowe@users.noreply.github.com>
2024-11-26 19:38:42 +0000

    Update Stroud CSCIC

--- data/scrapers/stroud_cscic.pl
+++ data/scrapers/stroud_cscic.pl
@@ -22,44 +22,43 @@ if(-e $file){
 	$str =~ s/[\n\r]+/\n/s;
 
 	@entries = ();
-
-	if($str =~ /<div class="wp-block-button">(.*?)<\/article>/s){
-		$str = $1;
-		while($str =~ s/<p><strong>(.*?)<\/strong>(.*?)<\/p>(.*?)(<p><strong>|<\/div>|<h2>)/$4/s){
-			$address = $1;
-			$entry = $3;
-			$address =~ s/^([^\,]+)\, ?//s;
-			$d = {'title'=>$1,'address'=>trimText(parseText($address))};
-
-			while($entry =~ s/<p>(.*?)<\/p>//){
-				$p = $1;
-				if($p =~ /Contact: (.*?)$/){
-					$contact = $1;
-					if($contact =~ /<a href="mailto:([^\"]+)"/){
-						$d->{'contact'} = "Email: $1";
-					}
-					if($contact =~ /<a href="(http[^\"]+)">/){
-						$d->{'url'} = $1;
-					}
-				}else{
-					$d->{'description'} .= ($d->{'description'} ? ", ":"").$p;
-				}
-			}
-			$day = "";
-			while($entry =~ s/<tr>(.*?)<\/tr>//){
-				$tr = $1;
-				@td = ();
-				while($tr =~ s/<td>(.*?)<\/td>//){
-					push(@td,trimText(parseText($1)));
-				}
-				if($td[0]){ $day = $td[0]; }
-				$d->{'hours'} .= ($d->{'hours'} ? "; ":"").parseText("$day $td[1]".($td[2] ? " ".$td[2]:""));
-			}
-			if($d->{'hours'}){
-				$d->{'hours'} = parseOpeningHours({'_text'=>$d->{'hours'}})
-			}
-			push(@entries,makeJSON($d,1));
+	
+	while($str =~ s/<h3 class="wp-block-heading"><strong>([^\>]+)<\/strong><\/h3>(.*?)(<h|<\/div>)//s){
+		$d = {'title'=>$1};
+		$entry = $2;
+		@lines = ();
+		while($entry =~ s/<p>(.*?)<\/p>//){
+			$line = $1;
+			$line =~ s/<\/?strong>//g;
+			$line =~ s/\&\#8211;/-/g;
+			push(@lines,$line);
 		}
+		$d->{'address'} = $lines[0];
+		$hours = "";
+		for($i = 1; $i < @lines; $i++){
+			if($lines[$i] =~ /(mon|tue|wed|thur|fri|sat|sun)/i){
+				$lines[$i] =~ s/<br ?\/?>/; /g;
+				$hours .= ($hours ? "; ":"").$lines[$i];
+			}elsif($lines[$i] =~ /Contact:<br>([0-9\s]+)/){
+				$d->{'contact'} .= ($d->{'contact'} ? "; ":"")."Tel: $1";
+				if($lines[$i] =~ s/<a href="mailto:([^\"]+)"[^\>]+>//){
+					$d->{'contact'} .= ($d->{'contact'} ? "; ":"")."Email: $1";
+				}
+				if(!$d->{'url'} && $lines[$i] =~ /<a href="([^\"]+)"/){
+					$d->{'url'} = $1;
+				}
+			}else{
+				$lines[$i] =~ s/<a href="([^\"]+)"[^\>]*>[^\<]*<\/a>//;
+				$d->{'description'} .= ($d->{'description'} && $d->{'description'} !~ /\.$/ ? ". ":"").trimHTML($lines[$i]);
+			}
+		}
+		if($hours){ 
+			$d->{'hours'} = parseOpeningHours({'_text'=>$hours});
+		}
+#		print Dumper @lines;
+#		print "HOURS=$hours\n";
+#		print Dumper $d;
+		push(@entries,makeJSON($d,1));
 	}
 
 	open(FILE,">:utf8","$file.json");
@@ -74,3 +73,18 @@ if(-e $file){
 
 }
 
+
+
+
+
+sub trimHTML {
+	my $str = $_[0];
+	$str =~ s/<br ?\/?>/\n/g;
+	$str =~ s/<[^\>]+>//g;
+	$str =~ s/(^[\s\t\n\r]+|[\s\t\n\r]+$)//g;
+	$str =~ s/[\n\r]{2,}/\n/g;
+	$str =~ s/[\s\t]{2,}/ /g;
+	$str =~ s/\.{2,}/\./g;
+	$str =~ s/\. ?\./\./g;
+	return $str;
+}
