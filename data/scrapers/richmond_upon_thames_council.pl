455a350bd03e0bf97c8b196a97c6659b8be8ed8b
slowe <299787+slowe@users.noreply.github.com>
2024-11-27 15:28:33 +0000

    Update Richmond upon Thames

--- data/scrapers/richmond_upon_thames_council.pl
+++ data/scrapers/richmond_upon_thames_council.pl
@@ -24,8 +24,9 @@ if(-e $file){
 		process '#warmspaces-directory .warmspace', "warmspaces[]" => scraper {
 			process 'h3', 'title' => 'HTML';
 			process 'p', 'p' => 'HTML';
-			process 'li', 'li[]' => 'HTML';
 			process 'span[class="loc"]', 'loc' => 'TEXT';
+			process '.serviceDetails div', 'details[]' => 'TEXT';
+			process 'ul.fac-list li', 'facilities[]' => 'HTML';
 		};
 	};
 
@@ -36,7 +37,7 @@ if(-e $file){
 	# Loop over warmspaces processing the <li> values
 	for($i = 0; $i < @{$res->{'warmspaces'}}; $i++){
 		$d = $res->{'warmspaces'}[$i];
-		$li = @{$d->{'li'}};
+		$li = @{$d->{'details'}};
 		$d->{'description'} = $d->{'p'};
 		delete $d->{'p'};
 		@coords = split(/,/,$d->{'loc'});
@@ -45,21 +46,33 @@ if(-e $file){
 		delete $d->{'loc'};
 		if($li > 0){
 			for($l = 0; $l < $li; $l++){
-				$d->{'li'}[$l] =~ s/[\n\r]//g;
-				if($d->{'li'}[$l] =~ /^Address: (.*)/){
+				$d->{'details'}[$l] =~ s/[\n\r]//g;
+				if($d->{'details'}[$l] =~ /^Address: ?(.*)/){
 					$d->{'address'} = $1;
 				}
-				if($d->{'li'}[$l] =~ /^Opening hours: (.*)/){
-					$d->{'hours'} = parseOpeningHours({'_text'=>$1});;
+				if($d->{'details'}[$l] =~ /^Opening hours: ?(.*)/){
+					$hours = $1;
+					if($hours ne "n/a"){
+						$d->{'hours'} = parseOpeningHours({'_text'=>$hours});
+					}
 				}
+				if($d->{'details'}[$l] =~ /^Telephone: ?(.*)/){
+					$d->{'contact'} .= ($d->{'contact'} ? "; ":"")."Tel: ".$1;
+				}
+				if($d->{'details'}[$l] =~ /^Email: ?(.*)/){
+					$d->{'contact'} .= ($d->{'contact'} ? "; ":"")."Email: ".$1;
+				}
+				if($d->{'details'}[$l] =~ /^Entry restrictions: ?(.*)/){
+					$entry = $1;
+					$d->{'description'} .= ($d->{'description'} ? " ":"").($entry eq "Open to all" ? "":"Entry restrictions: ").$1;
+				}
+				
 			}
-
-
 			# Remove the <li> entry
-			delete $d->{'li'};
+			delete $d->{'details'};
 			delete $d->{'p'};
-
 		}
+
 		# Store the entry as JSON
 		push(@entries,makeJSON($d,1));
 	}
