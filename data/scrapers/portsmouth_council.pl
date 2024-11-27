56433796b408b98564d6b29abbb1c2b865855ebd
slowe <299787+slowe@users.noreply.github.com>
2024-11-27 15:09:40 +0000

    Update Portsmouth

--- data/scrapers/portsmouth_council.pl
+++ data/scrapers/portsmouth_council.pl
@@ -17,31 +17,36 @@ if(-e $file){
 	open(FILE,"<:utf8",$file);
 	@lines = <FILE>;
 	close(FILE);
-	$str = join("",@lines);
+	$json = join("",@lines);
 
-
-	@entries;
-
-	while($str =~ s/<div class="content">(.*?)<\/div>//s){
-		$content = $1;
-
-		while($content =~ s/<p>(.*?)<\/p><ul>(.*?)<\/ul>//s){
-			$d = {'title'=>parseText($1)};
-			$ul = $2;
-			@li = ();
-			while($ul =~ s/<li>(.*?)<\/li>//s){
-				push(@li,$1);
-			}
-			$d->{'address'} = $li[0];
-			if($li[1]=~ /Opening/){ $d->{'hours'} = parseOpeningHours({'_text'=>parseText($li[1])}); }
-			if($li[2]){ $d->{'description'} = $li[2]; }
-			if($li[3] && $li[3] =~ /<a href="([^\"]+)">/){ $d->{'url'} = $1; }
-
-			# Store the entry as JSON
-			push(@entries,makeJSON($d,1));
+	if(!$json){ $json = "{}"; }
+	eval {
+		$json = JSON::XS->new->decode($json);
+	};
+	
+	for($m = 0; $m < @{$json->{'markers'}} ; $m++){
+		$d = {};
+		$d->{'title'} = $json->{'markers'}[$m]{'title'};
+		$d->{'address'} = $json->{'markers'}[$m]{'address'};
+		$d->{'lon'} = $json->{'markers'}[$m]{'lng'};
+		$d->{'lat'} = $json->{'markers'}[$m]{'lat'};
+		if($json->{'markers'}[$m]{'description'} =~ /href="([^\"]+)"/){
+			$d->{'url'} = $1;
 		}
-	}
+		$desc = $json->{'markers'}[$m]{'description'};
+		$hours = "";
+		while($desc =~ s/<p>(.*?)<\/p>//){
+			$line = $1;
+			if($line =~ /(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|holidays)/){
+				$hours .= ($hours ? "; ":"").$line;
+			}
+		}
+		if($hours){
+			$d->{'hours'} = parseOpeningHours({'_text'=>$hours});
+		}
 
+		push(@entries,makeJSON($d,1));
+	}
 	open(FILE,">:utf8","$file.json");
 	print FILE "[\n".join(",\n",@entries)."\n]";
 	close(FILE);
